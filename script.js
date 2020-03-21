c5d890aab5aec5782f4094bfd532e90a5a2985b3
sergiowork <sergioworkit@gmail.com>
2020-03-21 18:18:19 +0200

    feat: add slider navigation - 21.03.2020 19:18 по Минску

--- script.js
+++ script.js
@@ -42,3 +42,80 @@ var inputDescription = document.getElementById("description").value;
 	
 	});
 //END FORM
+
+//SLIDER
+        let items = document.querySelectorAll('.item');
+        let currentItem = 0;
+        let isEnabled = true;
+
+        function changeCurrentItem(n) {
+            currentItem = ( n  + items.length ) % items.length;
+        }
+
+        function hideItem(direction) {
+            isEnabled = false;
+            items[currentItem].classList.add(direction);
+            items[currentItem].addEventListener('animationend', function() {
+                this.classList.remove('activated', direction);
+            });
+        }
+        
+        function showItem(direction) {
+            items[currentItem].classList.add('next', direction);
+            items[currentItem].addEventListener('animationend', function() {
+                this.classList.remove('next', direction);
+                this.classList.add('activated');
+                isEnabled = true;
+            });
+        }
+
+        function previousItem(n) {
+            hideItem('to-right');
+            changeCurrentItem(n - 1);
+            showItem('from-left');
+            changeBackgroundColor();
+        }
+
+        function nextItem(n) {
+            hideItem('to-left');
+            changeCurrentItem(n + 1);
+            showItem('from-right');
+            changeBackgroundColor();
+        }
+       
+         function changeBackgroundColor() {
+            if(document.querySelector('.item').classList.contains('activated')) {
+                let slider = document.querySelector('.slider');
+                slider.style.backgroundColor = '#648bf0';
+            } else {
+            	let slider = document.querySelector('.slider');
+                slider.style.backgroundColor = '#f06c64';
+            }
+        }
+
+        document.querySelector('.arrow.arrow-left').addEventListener('click', function() {
+            if (isEnabled) {
+                previousItem(currentItem);
+            } 
+            });
+
+        document.querySelector('.arrow.arrow-right').addEventListener('click', function() {
+            if (isEnabled) {
+                nextItem(currentItem);
+            }
+        });
+
+// screen off phone
+       const verticalIphoneArea = document.querySelector('.vertical-screen');
+       const verticalblackScreen = document.querySelector('.iphonev-black');
+       const horizontalIphoneArea = document.querySelector('.horizontal-screen');
+       const horizontalblackScreen = document.querySelector('.iphoneh-black');
+
+       verticalIphoneArea.addEventListener('click', () => {
+       verticalblackScreen.classList.toggle('visible-screen');
+       });
+
+       horizontalIphoneArea.addEventListener('click', () => {
+       horizontalblackScreen.classList.toggle('visible-screen');
+       });
+//SLIDER
